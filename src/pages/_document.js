b1c84038859b57c137cdf2eef4c3cec35eec0352
Romain Billard <romain.billard@hetic.net>
2018-05-06 23:14:17 +0200

    Remove LayoutContainer and move some imports to `_app`

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,10 +2,6 @@ import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-// Import base styles
-import '../styles/reset.css'
-import '../styles/style.css'
-
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,15 +1,12 @@
 // @flow
 import React from 'react'
-import PageContainer from '@containers/PageContainer'
 import Wrapper from '@components/Wrapper'
 import RegistrationHeader from '@components/RegistrationHeader'
 import RegistrationForm from '@components/RegistrationForm'
 
 export default () => (
-  <PageContainer>
-    <Wrapper center width="small" mTop="enormous">
-      <RegistrationHeader />
-      <RegistrationForm />
-    </Wrapper>
-  </PageContainer>
+  <Wrapper center width="small" mTop="enormous">
+    <RegistrationHeader />
+    <RegistrationForm />
+  </Wrapper>
 )
