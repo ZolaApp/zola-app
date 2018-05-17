91130b61bdc60e63eb4690d30b1612b2d2676199
Tom Bonnike <bonniketom@gmail.com>
2018-05-17 10:53:06 +0200

    Update RegistrationForm markup

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -10,50 +10,76 @@ const RegistrationForm = () => {
   return (
     <Wrapper mTop="regular">
       <Box>
-        <form action="">
+        <form>
           <Wrapper mTop="regular">
             <DoubleInputsWrapper>
               <InputWithLabel
+                required
                 id="firstName"
                 label="First Name"
                 placeholder="John"
+                minLength={2}
+                maxLength={30}
               />
+
               <InputWithLabel
+                required
                 id="lastName"
                 label="Last Name"
                 placeholder="Doe"
+                minLength={2}
+                maxLength={30}
               />
             </DoubleInputsWrapper>
           </Wrapper>
+
           <Wrapper mTop="regular">
             <DoubleInputsWrapper>
               <InputWithLabel
+                required
                 id="job"
                 label="Job"
-                placeholder="Product Designer"
+                placeholder="Product owner"
+                minLength={2}
+                maxLength={50}
               />
+
               <InputWithLabel
+                required
                 id="email"
                 label="Email"
+                type="email"
                 placeholder="example@domain.com"
               />
             </DoubleInputsWrapper>
           </Wrapper>
-          <Wrapper mTop="regular">
-            <InputWithLabel id="password" label="Password" placeholder="****" />
-          </Wrapper>
+
           <Wrapper mTop="regular">
             <InputWithLabel
-              id="confirmPassword"
-              label="Confirm password"
-              placeholder="****"
+              required
+              id="password"
+              label="Password"
+              type="password"
+              placeholder="**********"
             />
           </Wrapper>
+
+          <Wrapper mTop="regular">
+            <InputWithLabel
+              required
+              id="confirmPassword"
+              label="Confirm password"
+              type="password"
+              placeholder="**********"
+            />
+          </Wrapper>
+
           <Wrapper mTop="large" mBottom="regular">
             <Button>Register a new account</Button>
           </Wrapper>
         </form>
       </Box>
+
       <Wrapper mTop="regular">
         <Box center>
           <InlineWrapper>
