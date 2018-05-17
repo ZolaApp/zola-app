b0061bbfddecdd58480880689f764453efa5306b
Romain Billard <romain.billard@hetic.net>
2018-05-17 10:11:58 +0200

    Rename KeysList component to KeyList

--- src/pages/project.js
+++ src/pages/project.js
@@ -3,14 +3,14 @@ import React from 'react'
 import Wrapper from '@components/Wrapper'
 import Sidebar from '@components/Sidebar'
 import ProjectSidebar from '@components/ProjectSidebar'
-import KeysList from '@components/KeysList'
+import KeyList from '@components/KeyList'
 
 export default () => (
   <Wrapper flex>
     <Sidebar />
     <ProjectSidebar />
     <Wrapper padding="xlarge" flex>
-      <KeysList />
+      <KeyList />
     </Wrapper>
   </Wrapper>
 )
