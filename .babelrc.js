dbf483a54f6525def6def9c38d9ee2bfbf799b4e
Tom Bonnike <bonniketom@gmail.com>
2018-05-18 11:25:16 +0200

    Pass down environment variables using .babelrc.js

---
+++ .babelrc.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+// Copy all environment variables loaded by `dotenv` to pass them to the
+// `transform-define` babel plugin.
+const environmentVariables = Object.entries(process.env).reduce(
+  (acc, [key, value]) => {
+    acc[`process.env.${key}`] = JSON.stringify(value)
+
+    return acc
+  },
+  {}
+)
+
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@components': './src/components',
+          '@containers': './src/containers',
+          '@higherOrders': './src/higherOrders',
+          '@pages': './src/pages'
+        }
+      }
+    ],
+    ['transform-define', { ...environmentVariables }],
+    ['styled-components', { ssr: true, displayName: true, preprocess: false }],
+    'inline-import-graphql-ast',
+    'transform-flow-strip-types'
+  ]
+}
