e1069d8d47e8f460d99f0bd98980365c8a04d08b
Romain Billard <romain.billard@hetic.net>
2018-05-24 10:33:55 +0200

    Add UI for projects stats

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Wrapper from '@components/Wrapper'
+import ProjectsStats from '@components/ProjectsStats'
 import ProjectsHeader from '@components/ProjectsHeader'
 import ProjectList from '@components/ProjectList'
 
 const Home = () => (
   <Wrapper flex>
     <Wrapper center width="regular" mTop="large">
+      <ProjectsStats />
       <ProjectsHeader />
       <ProjectList />
     </Wrapper>
