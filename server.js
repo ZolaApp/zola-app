bfaad09a803aa38d36bed8ed74e85fc8c5b6fbda
Tom Bonnike <bonniketom@gmail.com>
2018-05-24 13:44:34 +0200

    Add more accurate comment about environment variables in server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const dotenv = require('dotenv')
 const next = require('next')
 const routes = require('./routes')
 
-// This only loads the custom server’s environment variables.
+// This only loads the custom server’s environment variables. The app’s
+// environment variables are still loaded using the `transform-define` babel
+// plugin in `.babelrc.js`.
 dotenv.config()
 
 const app = next({ dev: process.env.NODE_ENV === 'development', dir: './src/' })
