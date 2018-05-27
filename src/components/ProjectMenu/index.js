014cd7246af49dab5fa4aa623dbedc96505804df
Tom Bonnike <bonniketom@gmail.com>
2018-05-27 17:22:20 +0200

    Render menu items as links

--- src/components/ProjectMenu/index.js
+++ src/components/ProjectMenu/index.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react'
+import Link from 'next/link'
 import { withRouter } from 'next/router'
 import Wrapper from '@components/Wrapper'
 import Text from '@components/Text'
@@ -7,21 +8,23 @@ import Icon from '@components/Icon'
 import { MenuItem } from './styles'
 
 const MENU_ITEMS = [
-  { pathname: '/project', icon: 'key', label: 'Keys' },
-  { pathname: '/locales', icon: 'planet', label: 'Locales' }
+  { href: '/', pathname: '/project', icon: 'key', label: 'Keys' },
+  { href: '/locales', pathname: '/locales', icon: 'planet', label: 'Locales' }
 ]
 
 type Props = { router: any, href: string }
 
 const ProjectMenu = ({ router, href }: Props) => (
   <Wrapper mTop="xlarge">
-    {MENU_ITEMS.map(({ pathname, icon, label }) => (
-      <MenuItem selected={router.pathname === pathname}>
-        <Icon icon={icon} />
-        <Text size="default" color="light">
-          {label}
-        </Text>
-      </MenuItem>
+    {MENU_ITEMS.map(({ href, pathname, icon, label }) => (
+      <Link href={`/project/${router.query.projectSlug + href}`}>
+        <MenuItem selected={router.pathname === pathname}>
+          <Icon icon={icon} />
+          <Text size="default" color="light">
+            {label}
+          </Text>
+        </MenuItem>
+      </Link>
     ))}
   </Wrapper>
 )
