104f8eca5ec6269a9cf01323f298baf8a824d9df
Tom Bonnike <bonniketom@gmail.com>
2018-05-27 17:27:15 +0200

    Improve handling of menu items’ links in the ProjectMenu component

--- src/components/ProjectMenu/index.js
+++ src/components/ProjectMenu/index.js
@@ -1,24 +1,24 @@
 // @flow
 import React from 'react'
-import Link from 'next/link'
 import { withRouter } from 'next/router'
 import Wrapper from '@components/Wrapper'
 import Text from '@components/Text'
 import Icon from '@components/Icon'
+import { Link } from '../../../routes'
 import { MenuItem } from './styles'
 
 const MENU_ITEMS = [
-  { href: '/', pathname: '/project', icon: 'key', label: 'Keys' },
-  { href: '/locales', pathname: '/locales', icon: 'planet', label: 'Locales' }
+  { route: 'project', icon: 'key', label: 'Keys' },
+  { route: 'locales', icon: 'planet', label: 'Locales' }
 ]
 
 type Props = { router: any, href: string }
 
 const ProjectMenu = ({ router, href }: Props) => (
   <Wrapper mTop="xlarge">
-    {MENU_ITEMS.map(({ href, pathname, icon, label }) => (
-      <Link href={`/project/${router.query.projectSlug + href}`}>
-        <MenuItem selected={router.pathname === pathname}>
+    {MENU_ITEMS.map(({ route, icon, label }) => (
+      <Link route={route} params={{ projectSlug: router.query.projectSlug }}>
+        <MenuItem selected={router.pathname === `/${route}`}>
           <Icon icon={icon} />
           <Text size="default" color="light">
             {label}
