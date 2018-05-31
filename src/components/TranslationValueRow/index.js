60f706f8b74684d6f6a153ce25738e860aeec542
Romain Billard <romain.billard@hetic.net>
2018-05-31 11:56:59 +0200

    Add key value textarea state management and styles

--- src/components/TranslationValueRow/index.js
+++ src/components/TranslationValueRow/index.js
@@ -1,22 +1,43 @@
 // @flow
-import React from 'react'
-import AddTranslationButton from '@components/AddTranslationButton'
+import React, { Component } from 'react'
 import { type Locale } from '@types/Locale'
-import { Wrapper, ContentWrapper, LocaleWrapper } from './styles'
+import { Wrapper, ContentWrapper, LocaleWrapper, Textarea } from './styles'
 
 type Props = {
   locale: Locale
 }
 
-const TranslationValueRow = ({ locale }: Props) => {
-  return (
-    <Wrapper>
-      <ContentWrapper>
-        <LocaleWrapper>{locale.name}</LocaleWrapper>
-        <AddTranslationButton />
-      </ContentWrapper>
-    </Wrapper>
-  )
+type State = {
+  isFocused: boolean
+}
+
+class TranslationValueRow extends Component<Props, State> {
+  state = {
+    isFocused: false
+  }
+
+  toggleFocus = () => {
+    this.setState(state => ({ isFocused: !state.isFocused }))
+  }
+
+  render() {
+    const { isFocused } = this.state
+    const { locale } = this.props
+
+    return (
+      <Wrapper>
+        <ContentWrapper isFocused={isFocused}>
+          <LocaleWrapper>{locale.name}</LocaleWrapper>
+          <Textarea
+            rows="1"
+            placeholder={`Translation for ${locale.name}`}
+            onFocus={this.toggleFocus}
+            onBlur={this.toggleFocus}
+          />
+        </ContentWrapper>
+      </Wrapper>
+    )
+  }
 }
 
 export default TranslationValueRow
--- src/components/TranslationValueRow/styles.js
+++ src/components/TranslationValueRow/styles.js
@@ -5,9 +5,10 @@ export const Wrapper = styled.div``
 
 export const ContentWrapper = styled.div`
   display: grid;
-  width: 55%;
-  grid-template-columns: 70px auto;
-  border: solid 1px ${({ theme }) => theme.colors.light};
+  grid-template-columns: 150px auto;
+  border: solid 1px
+    ${({ theme, isFocused }) =>
+      isFocused ? theme.colors.light : 'transparent'};
   padding: ${({ theme }) => theme.sizes.tiny};
   margin-bottom: ${({ theme }) => theme.sizes.tiny};
 
@@ -19,7 +20,8 @@ export const ContentWrapper = styled.div`
 
 export const LocaleWrapper = styled.div`
   display: inline-block;
-  width: 70px;
+  width: 150px;
+  align-self: start;
   text-align: center;
   background: ${({ theme }) => theme.colors.light};
   padding: ${({ theme }) => theme.sizes.tiny};
@@ -28,4 +30,29 @@ export const LocaleWrapper = styled.div`
   font-size: ${({ theme }) => theme.fonts.sizes.regular};
   font-weight: bold;
   color: ${({ theme }) => theme.colors.dark};
+`
+
+export const Textarea = styled.textarea`
+  padding: 0;
+  border: none;
+  background: transparent;
+  font-size: ${({ theme }) => theme.fonts.sizes.regular};
+  font-family: ${({ theme }) => theme.fonts.families.sourceSans};
+  line-height: 1.7em;
+  outline: none;
+  color: ${({ theme }) => theme.colors.light};
+  margin-left: ${({ theme }) => theme.sizes.regular};
+
+  &::-webkit-input-placeholder {
+    font-style: italic;
+  }
+  &:-moz-placeholder {
+    font-style: italic;
+  }
+  &::-moz-placeholder {
+    font-style: italic;
+  }
+  &:-ms-input-placeholder {
+    font-style: italic;
+  }
 `
