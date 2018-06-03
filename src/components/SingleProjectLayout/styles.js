c4ae44d1a19d2fe7ac5eebd84587d4c437e6f852
Romain Billard <romain.billard@hetic.net>
2018-06-03 15:45:30 +0200

    Add safari fix

--- src/components/SingleProjectLayout/styles.js
+++ src/components/SingleProjectLayout/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const SidebarsWrapper = styled.div`
   position: fixed;
   display: flex;
+  top: 0;
+  left: 0;
 `
 
 export const ContentWrapper = styled.div`
