5d699a2aa1d24b6580bc25346b35790a334bcead
Romain Billard <romain.billard@hetic.net>
2018-06-03 15:43:51 +0200

    Fix sidebar positioning styles

---
+++ src/components/SingleProjectLayout/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const SidebarsWrapper = styled.div`
+  position: fixed;
+  display: flex;
+`
+
+export const ContentWrapper = styled.div`
+  margin-left: 300px;
+  width: 100%;
+`
