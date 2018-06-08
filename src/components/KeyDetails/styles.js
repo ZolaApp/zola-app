c15778f99cec1cfeeb35022fb2480b19cbddcff1
Romain Billard <romain.billard@hetic.net>
2018-06-08 15:10:10 +0200

    Add border-radius to keyDetails

--- src/components/KeyDetails/styles.js
+++ src/components/KeyDetails/styles.js
@@ -7,5 +7,7 @@ export const DetailsWrapper = styled.div`
   border-top: solid 1px ${({ theme }) => theme.colors.alphaLight};
   background: ${({ theme }) => theme.colors.dark};
   color: ${({ theme }) => theme.colors.light};
-  border-bottom: solid 2px ${({ theme }) => theme.colors.light};
+  margin-bottom: 2px;
+  border-bottom-left-radius: ${({ theme }) => theme.globals.radius};
+  border-bottom-right-radius: ${({ theme }) => theme.globals.radius};
 `
