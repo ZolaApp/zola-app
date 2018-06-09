b9ee7d41688f7937d24d1094922b3d397b3a2077
Romain Billard <romain.billard@hetic.net>
2018-06-09 12:20:46 +0200

    Improve translation key row focus styles

--- src/components/TranslationValueRow/styles.js
+++ src/components/TranslationValueRow/styles.js
@@ -7,13 +7,11 @@ export const LocaleWrapper = styled.div`
   width: 150px;
   align-self: start;
   text-align: center;
-  background: ${({ theme }) => theme.colors.light};
   padding: ${({ theme }) => theme.sizes.tiny};
   border-radius: 2px;
   text-transform: uppercase;
   font-size: ${({ theme }) => theme.fonts.sizes.regular};
   font-weight: bold;
-  color: ${({ theme }) => theme.colors.dark};
 `
 
 export const LoaderWrapper = styled.div`
@@ -41,14 +39,21 @@ export const ContentWrapper = styled.div`
   grid-template-columns: 150px auto;
   border: solid 1px
     ${({ theme, isFocused }) =>
-      isFocused ? theme.colors.light : 'transparent'};
+      isFocused ? theme.colors.semiLight : 'transparent'};
   padding: ${({ theme }) => theme.sizes.tiny};
   margin-bottom: ${({ theme }) => theme.sizes.tiny};
-  border-radius: 2px;
+  border-radius: ${({ theme }) => theme.globals.radius};
 
   ${StyledButton} {
     justify-self: start;
     margin-left: ${({ theme }) => theme.sizes.tiny};
+  }
+
+  ${LocaleWrapper} {
+    color: ${({ isFocused, theme }) =>
+      isFocused ? theme.colors.dark : theme.colors.semiLight};
+    background: ${({ isFocused, theme }) =>
+      isFocused ? theme.colors.light : theme.colors.alphaLight};
   }
 
   &:hover {
