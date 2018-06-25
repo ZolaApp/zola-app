e74f840b32297f6cec8a30adef43e17315526b48
Romain Billard <romain.billard@hetic.net>
2018-06-25 09:23:09 +0200

    Translate projects list header

--- src/components/ProjectsHeader/index.js
+++ src/components/ProjectsHeader/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Wrapper from '@components/Wrapper'
 import Text from '@components/Text'
 import Button from '@components/Button'
+import { FormattedMessage } from 'react-intl'
 import { Texts, HeaderWrapper } from './styles'
 
 type Props = {
@@ -13,11 +14,15 @@ const ProjectsHeader = ({ onNewProjectClick }: Props) => (
   <Wrapper padding="large">
     <HeaderWrapper>
       <Texts>
-        <Text size="large">Your work</Text>
-        <Text color="semiDark">Projects you collaborate on</Text>
+        <Text size="large">
+          <FormattedMessage id="projects.header.title" />
+        </Text>
+        <Text color="semiDark">
+          <FormattedMessage id="projects.header.subtitle" />
+        </Text>
       </Texts>
       <Button onClick={onNewProjectClick} bordered>
-        Add new project
+        <FormattedMessage id="projects.header.add" />
       </Button>
     </HeaderWrapper>
   </Wrapper>
